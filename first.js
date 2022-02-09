6f37cdc13b9978da4a5518836883befdd924174d
Nick Kulway <nkulway@gmail.com>
2022-02-09 18:50:25 -0500

    began exercise via tsc's github

--- first.js
+++ first.js
@@ -1,15 +1,42 @@
 // let a = 5
 // let b = 5
 // let c = a + b
-// console.log(c)
-var Car = /** @class */ (function () {
-    function Car(model, doors, isElectric) {
-        this.model = model;
-        this.doors = doors;
-        this.isElectric = isElectric;
-    }
-    Car.prototype.displayMake = function () {
-        console.log("This car is ".concat(this.model));
-    };
-    return Car;
-}());
+// // console.log(c)
+// class Car {
+//   // fields  
+//     model: String;  
+//     doors: Number;  
+//     isElectric: Boolean;
+//   constructor(model: String, doors: Number, isElectric: Boolean) {  
+//       this.model = model;  
+//       this.doors = doors;  
+//       this.isElectric = isElectric;  
+//     }
+//   displayMake(): void {  
+//       console.log(`This car is ${this.model}`);  
+//     }
+//   }
+
+
+
+
+
+
+
+
+
+
+
+// 1. Avoid using "any" type at all costs.
+// 2. Difficulty quickly grows one exercise after another.
+// 3. Feel free to send pull-requests if you've come up
+//    with improvements!
+// 4. Provide feedback to the creator of these exercises.
+// 5. Enjoy.
+
+function printLabel(labeledObj) {
+    console.log(labeledObj.label);
+}
+var myObj = { size: 10, label: "Size 10 Object" };
+printLabel(myObj);
+
--- first.ts
+++ first.ts
@@ -2,23 +2,31 @@
 // let b = 5
 // let c = a + b
 
-// console.log(c)
+// // console.log(c)
 
-class Car {
+// class Car {
 
-  // fields  
-    model: String;  
-    doors: Number;  
-    isElectric: Boolean;
+//   // fields  
+//     model: String;  
+//     doors: Number;  
+//     isElectric: Boolean;
   
-  constructor(model: String, doors: Number, isElectric: Boolean) {  
-      this.model = model;  
-      this.doors = doors;  
-      this.isElectric = isElectric;  
-    }
+//   constructor(model: String, doors: Number, isElectric: Boolean) {  
+//       this.model = model;  
+//       this.doors = doors;  
+//       this.isElectric = isElectric;  
+//     }
   
-  displayMake(): void {  
-      console.log(`This car is ${this.model}`);  
-    }
+//   displayMake(): void {  
+//       console.log(`This car is ${this.model}`);  
+//     }
   
+//   }
+
+
+  function printLabel(labeledObj: { label: string }) {
+    console.log(labeledObj.label);
   }
+  
+  let myObj = { size: 10, label: "Size 10 Object" };
+  printLabel(myObj);
