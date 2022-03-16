9aa5de96a503272f2d8e537aa978b6c3c7cd579a
Nick Kulway <nkulway@gmail.com>
2022-03-16 18:15:40 -0400

    created void function with ts

---
+++ types.js
@@ -0,0 +1,19 @@
+"use strict";
+// var n : Number = 1 // as opposed to String or any
+// n = 'Rob'
+var isWinter = false;
+var count = 5;
+var name = 'Bryn';
+var names = ["John", "Rickon", "Arya"];
+var Starks;
+(function (Starks) {
+    Starks[Starks["Jon"] = 0] = "Jon";
+    Starks[Starks["Bran"] = 1] = "Bran";
+    Starks[Starks["Eddard"] = 2] = "Eddard";
+    Starks[Starks["Catlyn"] = 3] = "Catlyn";
+})(Starks || (Starks = {}));
+var cat = Starks.Catlyn;
+function getName() {
+    console.log('Winter is coming!');
+}
+module.exports = {};
---
+++ types.ts
@@ -0,0 +1,20 @@
+export = {}
+
+// var n : Number = 1 // as opposed to String or any
+
+// n = 'Rob'
+
+var isWinter : boolean = false
+
+var count : Number = 5
+var name : String = 'Bryn'
+
+var names : string[] = ["John", "Rickon", "Arya"]
+
+enum Starks {Jon, Bran, Eddard, Catlyn}
+
+var cat : Starks = Starks.Catlyn
+
+function getName() : void{
+  console.log('Winter is coming!')
+}
